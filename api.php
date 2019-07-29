fea3e0b12db8468f0b670b0776c0042748b454cb
RafSwiggers <rafswiggers@gmail.com>
2019-07-29 15:29:54 +0200

    add the access control header

--- api.php
+++ api.php
@@ -1,7 +1,7 @@
 <?php
+header("Access-Control-Allow-Origin: *");
 $array = array();
-
-if($_GET["numbers"] === null){
+if($_GET["numbers"] === NULL){
     for ($x = 0; $x <= 5; $x++) {
         array_push($array, array(rand($x, 100), rand(0, 100)));
     };
