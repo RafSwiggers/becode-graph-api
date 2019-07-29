8c65a6e8da936bf754eb559ccb29c23bb1c348e7
RafSwiggers <rafswiggers@gmail.com>
2019-07-29 15:03:26 +0200

    add conditional for empty query

--- api.php
+++ api.php
@@ -1,8 +1,16 @@
 <?php
 $array = array();
-for ($x = 0; $x <= $_GET["numbers"]? : 5; $x++) {
-    array_push($array, array(rand($x, 100), rand(0, 100)));
+
+if($_GET["numbers"] === null){
+    for ($x = 0; $x <= 5; $x++) {
+        array_push($array, array(rand($x, 100), rand(0, 100)));
+    };
+} else {
+    for ($x = 0; $x <= $_GET["numbers"]; $x++) {
+        array_push($array, array(rand($x, 100), rand(0, 100)));
+    };
 };
+
 echo json_encode(($array));
 
 ?>
