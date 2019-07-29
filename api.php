6fb2c2769730663464a606856b6df98e735470b4
RafSwiggers <rafswiggers@gmail.com>
2019-07-29 15:03:00 +0200

    add the if condition for empty query

--- api.php
+++ api.php
@@ -1,6 +1,6 @@
 <?php
 $array = array();
-for ($x = 0; $x <= $_GET["numbers"]; $x++) {
+for ($x = 0; $x <= $_GET["numbers"]? : 5; $x++) {
     array_push($array, array(rand($x, 100), rand(0, 100)));
 };
 echo json_encode(($array));
