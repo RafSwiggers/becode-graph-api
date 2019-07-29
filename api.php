08d4306d0a58795dcc469596b577042ff9c6b105
RafSwiggers <rafswiggers@gmail.com>
2019-07-29 14:48:23 +0200

    add the GET query to the code

--- api.php
+++ api.php
@@ -1,7 +1,8 @@
 <?php
 $array = array();
-echo "Hello world \n";
-for ($x = 0; $x <= 10; $x++) {
-    array_push($array, rand(0, 100));
+for ($x = 0; $x <= $_GET["numbers"]; $x++) {
+    array_push($array, array(rand($x, 100), rand(0, 100)));
 };
 echo json_encode(($array));
+
+?>
